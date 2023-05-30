e66961d9340b9ad4b6a351879b046ba7387a3119
joelchitala <chitalahjoel@gmail.com>
2023-05-30 13:58:47 +0400

    added files

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ client.connect(uri,(err,cli)=>{
 app.use(cors())
 app.use((req,res,next)=>{
     let dt = new Date();
-    let formatted_date =
-      `${dt.getFullYear()} - ${dt.getMonth() + 1} - ${dt.getDate()}
-       ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}
-      `
+    let formatted_date = `${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`
     let formatted_log = `[${formatted_date}] ${req.method}:${req.url} ${res.statusCode}`;
     console.log(formatted_log);
     next()
